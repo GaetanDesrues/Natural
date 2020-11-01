858e8c7ab8449c6206a2ce207e53c316b95d18f0
Gaetan <gdesrues@gmail.com>
2020-11-01 15:13:07 +0100

    ok

---
+++ fcm.py
@@ -0,0 +1,16 @@
+from pyfcm import FCMNotification
+from db import get_tokens
+from constants import API_KEY
+
+
+def send_notif(message_title, message_body):
+    push_service = FCMNotification(api_key=API_KEY)
+    registration_ids = get_tokens()
+
+    result = push_service.notify_multiple_devices(
+        registration_ids=registration_ids,
+        message_title=message_title,
+        message_body=message_body,
+    )
+
+    return result
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import requests
 import smtplib, ssl
 from log_file import getLog, LOG_LEVEL, LOG_FILE
 from constants import from_address, password, to_contact, headers
+from fcm import send_notif
 
 log = getLog(__name__)
 log.info(f"This app logs at level logging.{LOG_LEVEL} to file://{LOG_FILE}")
 
-reservation = dict(node="7736", guichets=["14510", "14520", "16456", "17481"], name="Reservation")
+reservation = dict(
+    node="7736", guichets=["14510", "14520", "16456", "17481"], name="Reservation"
+)
 sur_reservation = dict(node="18131", guichets=["18826"], name="Sur Reservation")
 
 URL = "https://www.haute-garonne.gouv.fr/booking/create/{}/1"
@@ -16,7 +19,9 @@ phrase = "Il n'existe plus de plage horaire libre pour votre demande de rendez-v
 def test_all():
     for tp in [reservation, sur_reservation]:
         for p in tp["guichets"]:
-            r = make_request(tp["node"], {"planning": p, "nextButton": "Etape+suivante"})
+            r = make_request(
+                tp["node"], {"planning": p, "nextButton": "Etape+suivante"}
+            )
             test_response(r, tp["name"])
 
 
@@ -30,42 +35,51 @@ def make_request(node, data):
 
 def test_response(r, context):
     if r is None:
+        log.error(f"Internal python error")
         return
 
     if r.status_code == 502:
         log.error(f"Error 502: {r.url}")
+        make_notif(r.url, "ERROR 502")
         return
 
     if phrase in r.text:
-        log.debug(f"Ok but no rdv dispo ({r.url})")#, r.text)
+        log.debug(f"Ok but no rdv dispo ({r.url})")  # , r.text)
     else:
         # log.info(r.text)
         make_mail(r.url, context)
+        make_notif(r.url, context)
 
 
 def make_mail(url, context):
     msg = f"Un guichet semble etre disponible en {context}, visit {url}"
     log.info(msg)
 
-    message = ''.join(
-        'From: Gaga <gaga@example.com>\n'
-        'To: <you@example.com>\n'
-        'Subject: Bonne nouvelle: un guichet est libre\n'
-        '\n'
-        f'{msg}\n'
+    message = "".join(
+        "From: Gaga <gaga@example.com>\n"
+        "To: <you@example.com>\n"
+        "Subject: Bonne nouvelle: un guichet est libre\n"
+        "\n"
+        f"{msg}\n"
     )
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(from_address, password)
         server.sendmail(
-            from_address,
-            to_contact,
-            message,
+            from_address, to_contact, message,
         )
         log.info("Mail envoye")
 
 
+def make_notif(url, context):
+    title = "Bonne nouvelle: un guichet semble libre"
+    msg = f"Un guichet semble etre disponible en {context}"
 
-if __name__=="__main__":
+    r = send_notif(title, msg)
+
+    log.info(f"Notification sent: {r}")
+
+
+if __name__ == "__main__":
     test_all()
