489a4fa8613c51246eb816a3ed8b154547c0e09f
Gaetan <gdesrues@gmail.com>
2020-11-15 04:20:19 +0100

    release

--- __main__.py
+++ __main__.py
@@ -2,14 +2,3 @@ from modules.parse import GouvParser
 
 if __name__ == "__main__":
     GouvParser()
-
-    # infos = dict(
-    #     date=now(),
-    #     name=SRES.name,
-    #     node=SRES.node,
-    #     planning=1234,
-    #     res=True,
-    #     date_last_success=now(),
-    #     reason="Guichet no5 (Haute-Garonne) -- Checked but nope",
-    # )
-    # make_notif(infos)
